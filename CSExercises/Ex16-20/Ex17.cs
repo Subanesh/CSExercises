adfbe6d7c2db5c0b512d27f1c87ba4a53bc59959
Subanesh <suban0689@gmail.com>
2015-08-17 12:18:25 +0800

    completed

--- CSExercises/Ex16-20/Ex17.cs
+++ CSExercises/Ex16-20/Ex17.cs
@@ -1,10 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace CSExercises
 {
     public class Ex17
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Gender:");
+            string gender = Console.ReadLine();
+            Console.WriteLine("Age:");
+            int age = int.Parse(Console.ReadLine());
+
+
+            if (gender == "M")
+            {
+                if( age>=40)
+                {
+                    Console.WriteLine("Good Morning Uncle {0}", name);
+                }
+                else
+                {
+                    Console.WriteLine("Good Morning Mr. {0}", name);
+                }
+            }
+            else if (gender == "F")
+            {
+                if (age >= 40)
+                {
+                    Console.WriteLine("Good Morning lady {0}", name);
+                }
+                else
+                {
+                    Console.WriteLine("Good Morning Ms. {0}", name);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Enter Gender Like M or F");
+            }
+
+            Console.ReadLine();
         }
     }
 }
--- CSExercises/Ex16-20/Ex18.cs
+++ CSExercises/Ex16-20/Ex18.cs
@@ -1,10 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace CSExercises
 {
     public class Ex18
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter Marks between 0 and 100");
+            int m = int.Parse(Console.ReadLine());
+            if (m >= 80 && m <= 80)
+            {
+                Console.WriteLine("You Scored {0} marks Which is A grade",m);
+
+            }
+
+            else if(m>=60 && m<=79)
+            {
+                Console.WriteLine("You Scored {0} marks Which is B grade",m);
+            }
+
+            else if (m >= 40 && m <= 59)
+            {
+                Console.WriteLine("You Scored {0} marks Which is C grade",m);
+            }
+            else if (m>= 0 && m <= 39)
+            {
+                Console.WriteLine("You Scored {0} marks Which is F grade", m);
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Marks between 0 and 100 ",m);
+            }
+
+            Console.ReadLine();
         }
     }
 }
