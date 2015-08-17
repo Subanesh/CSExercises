842c9c1f71c16fd15c0ed1af9e117a3127e58aa8
Subanesh <suban0689@gmail.com>
2015-08-17 13:54:22 +0800

    completed

--- CSExercises/Ex07-15/Ex12.cs
+++ CSExercises/Ex07-15/Ex12.cs
@@ -15,9 +15,9 @@ namespace CSExercises
             double d = double.Parse(Console.ReadLine());
 
             double f = 2.40 + (d * 0.4);
-            double ff = Math.Round(f / 10) * 10;
+            double ff = Math.Ceiling(f * 10) / 10;
 
-            Console.WriteLine("Total Fare:{0:0.00}", f);
+            Console.WriteLine("Total Fare:{0:0.00}", ff);
 
             Console.ReadLine();
         }
--- CSExercises/Ex16-20/Ex19.cs
+++ CSExercises/Ex16-20/Ex19.cs
@@ -1,10 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace CSExercises
 {
     public class Ex19
     {
         public static void Main(string[] args)
         {
-            
+            double minimum = 2.40,
+                  every100 = 0.04,
+                  after100 = 0.05,
+                  calculation;
+            Console.WriteLine("Travel Distance:");
+            double distance = double.Parse(Console.ReadLine());
+
+
         }
     }
 }
