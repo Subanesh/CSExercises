316499feead118c746de6571b5329fcdb8edc519
Subanesh <suban0689@gmail.com>
2015-08-17 16:01:24 +0800

    Completed

--- CSExercises/Ex16-20/Ex19.cs
+++ CSExercises/Ex16-20/Ex19.cs
@@ -10,12 +10,25 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            double minimum = 2.40,
-                  every100 = 0.04,
-                  after100 = 0.05,
+            double 
                   calculation;
             Console.WriteLine("Travel Distance:");
             double distance = double.Parse(Console.ReadLine());
+
+           double distance1 = Math.Ceiling(distance * 10) / 10;
+
+           if (distance1 < 9)
+           {
+               calculation = 2.40 + (distance1 * 10) * 0.04;
+           }
+           else
+           {
+               calculation = 2.40 + (85 * 0.04) + ((distance1 - 9) * 10) * 0.05;
+           }
+
+           Console.WriteLine("Charge:{0:c}", calculation);
+
+           Console.ReadLine();
 
 
         }
--- CSExercises/Ex16-20/Ex20.cs
+++ CSExercises/Ex16-20/Ex20.cs
@@ -1,10 +1,52 @@
-﻿namespace CSExercises
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSExercises
 {
     public class Ex20
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter Quantity of TV,DVD,Mp3 Respectivily:");
+
+            int tv = int.Parse(Console.ReadLine());
+            int dvd = int.Parse(Console.ReadLine());
+            int mp3 = int.Parse(Console.ReadLine());
+
+            double total = (tv * 900) + (dvd * 500) + (mp3 * 700);
+
+            if (total >= 5000 && total <= 10000)
+            {
+                double dtv = (tv * 900 * 10) / 100;
+                double ddvd = (dvd * 500 * 10) / 100;
+
+                double tdiscount = total - (dtv + ddvd);
+
+                Console.WriteLine("Total Price After Discount:{0:c}", tdiscount);
+            }
+
+           else if(total > 10000 )
+
+            {
+                double dtv = (tv * 900 * 15) / 100;
+                double ddvd = (dvd * 500 * 15) / 100;
+
+                double tdiscount = total - (dtv + ddvd);
+
+                Console.WriteLine("Total Price After Discount:{0:c}", tdiscount);
+           }
+
+            else
+            {
+                Console.WriteLine("Total Price:{0:c}", total);
+
+            }
+
+            Console.ReadLine();
+ 
         }
     }
 }
